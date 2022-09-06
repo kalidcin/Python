fa6ec1fe3fb39723a874f3abb852977b52898d76
kalidcin <valteer.kalid@gmail.com>
2022-09-06 13:21:54 -0500

    Clases

---
+++ POO/MetodosAtributos.py
@@ -0,0 +1,13 @@
+class FabricaTelefonos():
+    marca = "Huawei"
+    color = "Negro"
+    memoriaRam = 32
+    almacenamiento = 128
+
+telefono = FabricaTelefonos()
+telefono.marca # Atributo
+telefono.color # Atributo
+telefono.memoriaRam # Atributo
+telefono.almacenamiento # Atributo
+
+print(telefono.almacenamiento)
