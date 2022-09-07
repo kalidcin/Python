370b627575e901e2b3ecce0ea9909e3fe93c1c4d
kalidcin <valteer.kalid@gmail.com>
2022-09-07 11:40:19 -0500

    Metodos

--- POO/MetodosAtributos.py
+++ POO/MetodosAtributos.py
@@ -4,10 +4,13 @@ class FabricaTelefonos():
     memoriaRam = 32
     almacenamiento = 128
 
+    def llamar(self, mensaje):
+        print(mensaje)
+
 telefono = FabricaTelefonos()
 telefono.marca # Atributo
 telefono.color # Atributo
 telefono.memoriaRam # Atributo
 telefono.almacenamiento # Atributo
 
-print(telefono.almacenamiento)
+telefono.llamar("Hola, ¿con quién hablo?")
